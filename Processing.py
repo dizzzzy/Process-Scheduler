df7b8d82eff63baa5ace440a600a04b5476c7fbb
dizzzzy <dimitriosziavras@hotmail.com>
2017-04-09 10:13:26 -0400

    James' branch

--- Processing.py
+++ Processing.py
@@ -1,29 +1,27 @@
-import threading
-import time
-
-
-
-class ProcessThread(threading.Thread):
-    def __init__(self, process_object):
-        super(ProcessThread, self).__init__()
-        self.process = process_object
-        self.process_name = process_object.get_name()
-        self.paused = False
-        self.hasStarted = False
-        self.pause_cond = threading.Condition(threading.Lock())
-
-    def pause(self, pauseTime):
-        self.paused = True
-        self.pause_cond.acquire()
-        print self.process_name + " process has paused at " + str(pauseTime)
-
-    def resume(self, resumeTime):
-        self.paused = False
-        self.pause_cond.release()
-        print self.process_name + " process has resumed at " + str(resumeTime)
-
-    def start(self, startTime):
-        print self.process_name + " process has started at " + str(startTime)
-        super(ProcessThread, self).start()
-
-        self.hasStarted = True
+# import threading
+#
+#
+#
+# class ProcessThread(threading.Thread):
+#     def __init__(self, process_object):
+#         super(ProcessThread, self).__init__()
+#         # self.process = process_object
+#         # self.process_name = process_object.get_name()
+#         # self.paused = False
+#         # self.hasStarted = False
+#         # self.pause_cond = threading.Condition(threading.Lock())
+#
+#     def pause(self, pauseTime):
+#         self.paused = True
+#         self.pause_cond.acquire()
+#         print self.process_name + " process has paused at " + str(pauseTime)
+#
+#     def resume(self, resumeTime):
+#         self.paused = False
+#         self.pause_cond.release()
+#         print self.process_name + " process has resumed at " + str(resumeTime)
+#
+#     def start(self, startTime):
+#         print self.process_name + " process has started at " + str(startTime)
+#         super(ProcessThread, self).start()
+#         self.hasStarted = True
